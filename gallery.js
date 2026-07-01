55fff91e65b2fdedf86646bd3fcf6adbff5e58c9
Leeeonieee <leonie.picker@hotmail.com>
2026-07-01 19:27:07 +1000

    Update fetch URL in loadData function

--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@ let loadedCount = 0;
 const BATCH_SIZE = 20;
 
 async function loadData() {
-  const res = await fetch("https://script.google.com/macros/s/AKfycbwFa1OfbwLLuW5NDN43x5mUSc7f91DW1h7X7aSIobF7_Hv9mVquziFZopHEOUFgbpcVGw/exec");
+  const res = await fetch("https://script.google.com/macros/s/AKfycbw9TEFO4HPb3KkvU1GsIgXWYqv1JsyZbwVbvRzgZ5f0d7cnVolsEcbsGLpRoYnhMtMigA/execc");
   allData = await res.json();
 
   applyFilter();
